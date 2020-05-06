e50811d9275dc008e5d8a7d75f98d4ac7b5e1cb5
Robert Steele <stryakr@gmail.com>
2020-05-06 09:25:58 -0700

    doc changes

---
+++ Examples/Chaining/A1_QueueAlert/index.js
@@ -0,0 +1,10 @@
+﻿
+/**
+ * The function.json for this activity doesn't use the context bindings,
+ * rather `$return` for the named output. The associated documentation for $return behavior
+ * https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-return-value?tabs=javascript
+ */
+async function queueAlert(context, alert) {
+  return alert;
+}
+module.exports = queueAlert;
